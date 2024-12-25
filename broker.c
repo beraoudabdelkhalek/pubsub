43006719173e62465b40a67b5db33ae0ee8e23b2
beraoudabdelkhalek <ka_beraoud@esi.dz>
2024-12-25 20:52:30 +0100

    fixed broker

--- broker.c
+++ broker.c
@@ -4,20 +4,16 @@
 
 int main() {
     void *context = zmq_ctx_new();
-    printf("Broker is running using zmq proxy\n");
     // XSUB socket for publishers
     void *xsub_socket = zmq_socket(context, ZMQ_XSUB);
-    printf("Broker is running using zmq proxy1\n");
     zmq_bind(xsub_socket, "tcp://*:5555");
     
     // XPUB socket for subscribers
     void *xpub_socket = zmq_socket(context, ZMQ_XPUB);
     zmq_bind(xpub_socket, "tcp://*:5556");
-    printf("Broker is running using zmq proxy2\n");
-
+    printf("Broker is running using zmq proxy\n");
     // Proxy between XSUB and XPUB sockets
     zmq_proxy(xsub_socket, xpub_socket, NULL);
-    printf("Broker is running using zmq proxy\n");
 
     // Clean up
     zmq_close(xsub_socket);
